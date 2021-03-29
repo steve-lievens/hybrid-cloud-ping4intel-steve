6f2038c6198195e089b09a96451915c62c92d1a1
Bataces <baljit.singh@student.ucll.com>
2021-03-29 16:24:02 +0200

    initial commit project setup

---
+++ src/test/java/com/team4/hybridcloudping4intel/HybridCloudPing4intelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team4.hybridcloudping4intel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HybridCloudPing4intelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
