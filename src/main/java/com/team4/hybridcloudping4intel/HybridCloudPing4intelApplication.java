2e7709f2d4c4981ae976c5435ff19719e5221250
kevin <kevin.rooseboom@student.howest.be>
2021-03-30 13:07:08 +0200

    add startupcode for encryption

--- src/main/java/com/team4/hybridcloudping4intel/HybridCloudPing4intelApplication.java
+++ src/main/java/com/team4/hybridcloudping4intel/HybridCloudPing4intelApplication.java
@@ -2,12 +2,24 @@ package com.team4.hybridcloudping4intel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import util.Crypto;
 
 @SpringBootApplication
 public class HybridCloudPing4intelApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HybridCloudPing4intelApplication.class, args);
+        new HybridCloudPing4intelApplication().run();
     }
+
+    public void run(){
+        String username = Crypto.getInstance().encrypt("");
+        System.out.println(username);
+
+        String pw = Crypto.getInstance().encrypt("");
+        System.out.println(pw);
+    }
+
+
 
 }
